492146af9a0d1c5c3c4ecb442bc3f615ec9faa03
Mike Romanov <romanov.michael.v@gmail.com>
2026-06-04 00:15:22 +0200

    Added Autoencoder homework task

--- .laborantum/src/models/feedforward/autoencoder.py
+++ .laborantum/src/models/feedforward/autoencoder.py
@@ -6,99 +6,49 @@ class Autoencoder(torch.nn.Module):
             channels,
             activation=torch.nn.ReLU):
         ...
-        ## YOUR CODE HERE
         super().__init__()
-
+        ## YOUR CODE HERE
+        # -- placeholder start --
         encoder_layers = []
-        for index in range(len(channels) - 1):
+        for index in range(len(channels) - 2):
             encoder_layers.append(torch.nn.Linear(channels[index], channels[index + 1]))
             encoder_layers.append(activation())
-        encoder_layers.pop()
+        encoder_layers.append(torch.nn.Linear(channels[-2], channels[-1]))
 
         self.encoder = torch.nn.Sequential(*encoder_layers)
 
         decoder_layers = []
-        channels = channels[::-1]
-        for index in range(len(channels) - 1):
-            decoder_layers.append(torch.nn.Linear(channels[index], channels[index + 1]))
+        decoder_channels = channels[::-1]
+        for index in range(len(decoder_channels) - 2):
+            decoder_layers.append(torch.nn.Linear(decoder_channels[index], decoder_channels[index + 1]))
             decoder_layers.append(activation())
-        decoder_layers.pop()
+        decoder_layers.append(torch.nn.Linear(decoder_channels[-2], decoder_channels[-1]))
 
         self.decoder = torch.nn.Sequential(*decoder_layers)
+        # -- placeholder end --
+        if not hasattr(self, 'encoder'):
+            self.encoder = torch.nn.Identity()
+        if not hasattr(self, 'decoder'):
+            self.decoder = torch.nn.Identity()
 
-    def __call__(self, signal):
+    def __forward_kernel(self, signal):
         input_shape = signal.shape
         res = signal
         ## YOUR CODE HERE
+        # -- placeholder start --
         res = res.reshape([res.shape[0], -1])
         res = self.encoder(res)
         res = self.decoder(res)
+        # -- placeholder end --
         res = res.reshape(input_shape)
         return res
 
-
-class Sampler(torch.nn.Module):
-    def __init__(self, channels):
-        ...
+    def forward(self, batch):
         ## YOUR CODE HERE
-        super().__init__()
-        self.mu_regressor = torch.nn.Linear(channels, channels)
-        self.logvar_regressor = torch.nn.Linear(channels, channels)
-
-
-    def __call__(self, signal):
-        res = signal
-        mu = signal
-        sigma = signal
-
-        ## YOUR CODE HERE
-        mu = self.mu_regressor(signal)
-        logvar = self.logvar_regressor(signal)
-
-        sigma = (logvar / 2).exp()
-
-        if self.training:
-            noise = torch.randn_like(mu)
-            res = noise * sigma + mu
-        else:
-            res = mu
-        return res, mu, sigma
-
-
-class VAE(torch.nn.Module):
-    def __init__(
-            self,
-            channels,
-            activation=torch.nn.ReLU):
-        ...
-        ## YOUR CODE HERE
-        super().__init__()
-
-        encoder_layers = []
-        for index in range(len(channels) - 1):
-            encoder_layers.append(torch.nn.Linear(channels[index], channels[index + 1]))
-            encoder_layers.append(activation())
-        encoder_layers.pop()
-
-        self.encoder = torch.nn.Sequential(*encoder_layers)
-        self.sampler = Sampler(channels[-1])
-
-        decoder_layers = []
-        channels = channels[::-1]
-        for index in range(len(channels) - 1):
-            decoder_layers.append(torch.nn.Linear(channels[index], channels[index + 1]))
-            decoder_layers.append(activation())
-        decoder_layers.pop()
-
-        self.decoder = torch.nn.Sequential(*decoder_layers)
-
-    def __call__(self, signal):
-        input_shape = signal.shape
-        res = signal
-        ## YOUR CODE HERE
-        res = res.reshape([res.shape[0], -1])
-        res = self.encoder(res)
-        res, mu, sigma = self.sampler(res)
-        res = self.decoder(res)
-        res = res.reshape(input_shape)
-        return res, mu, sigma
+        # -- placeholder start --
+        reconstruction = self.__forward_kernel(batch['data']['image'])
+        batch['signals'] = {'reconstruction': reconstruction}
+        # -- placeholder end --
+        if 'signals' not in batch:
+            batch['signals'] = {'reconstruction': batch['data']['image']}
+        return batch
